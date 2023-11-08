6359d9e4ad4d0ff5b8cbd3a39f5418fc113305c6
Parth Joshi <pjoshi08@github.com>
2023-11-08 15:32:48 -0500

    codelab start

--- app/src/main/java/com/example/modernplayground/MainActivity.kt
+++ app/src/main/java/com/example/modernplayground/MainActivity.kt
@@ -10,37 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.example.modernplayground.ui.theme.CupCakeTheme
 import com.google.accompanist.appcompattheme.AppCompatTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            AppCompatTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            CupCakeTheme {
+                CupcakeApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppCompatTheme {
-        Greeting("Android")
     }
 }
