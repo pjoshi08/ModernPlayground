461b8363e55b5ec1c897315f15f024620848cb54
Parth Joshi <pjoshi08@github.com>
2023-12-01 15:35:55 -0500

    state codelab end

--- app/src/main/java/com/example/modernplayground/MainActivity.kt
+++ app/src/main/java/com/example/modernplayground/MainActivity.kt
@@ -10,37 +10,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.modernplayground.ui.theme.BasicStateCodelabTheme
 import com.google.accompanist.appcompattheme.AppCompatTheme
 
+/// Compose Documentation: https://developer.android.com/jetpack/compose/mental-model
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppCompatTheme {
+            BasicStateCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppCompatTheme {
-        Greeting("Android")
     }
 }
